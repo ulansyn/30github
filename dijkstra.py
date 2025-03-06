b5d3129d2dbb71185db4dda1f53800e69174850a
Ulansyn Toktorbai uulu <100096643+ulansyn@users.noreply.github.com>
2025-03-06 23:21:45 +0600

    Create dijkstra.py

---
+++ dijkstra.py
@@ -0,0 +1,31 @@
+import heapq
+
+def dijkstra(graph, start):
+    distances = {vertex: float('infinity') for vertex in graph}
+    distances[start] = 0
+    priority_queue = [(0, start)]
+    
+    while priority_queue:
+        current_distance, current_vertex = heapq.heappop(priority_queue)
+        if current_distance > distances[current_vertex]:
+            continue
+        for neighbor, weight in graph[current_vertex].items():
+            distance = current_distance + weight
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(priority_queue, (distance, neighbor))
+    return distances
+
+if __name__ == "__main__":
+    graph = {
+        'A': {'B': 1, 'C': 4},
+        'B': {'A': 1, 'C': 2, 'D': 5},
+        'C': {'A': 4, 'B': 2, 'D': 1},
+        'D': {'B': 5, 'C': 1}
+    }
+    
+    start_vertex = 'A'
+    distances = dijkstra(graph, start_vertex)
+    print(f"Кратчайшие расстояния от вершины {start_vertex}:")
+    for vertex, distance in distances.items():
+        print(f"До {vertex}: {distance}")
